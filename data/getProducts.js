d1f1a776eddc6f544a1fd132c6ed04c4d41fd47b
Nehal <nehal@astrumlabs.co>
2020-01-03 15:46:55 +0530

    List product city, state and country data

--- data/getProducts.js
+++ data/getProducts.js
@@ -52,87 +52,15 @@ module.exports = () => {
             id: "5da955e24ab832999059527a"
           }
         ]
-      },
-      {
-        id: "5dc5388a58de1e007b84c604",
-        name: "Automatic Fly Ash Brick Plant",
-        minQuantity: "1pc",
-        categoryId: 1,
-        subCategoryId: 2,
-        productTypeId: 3,
-        sellerId: "eb30dfce-a2a3-49ef-8f86-eda48c636f1b",
-        images: [
-          {
-            actualSizeUrl:
-              "http://localhost:10000/devstoreaccount1/productimages/5dc5388a58de1e007b84c604/5da95615dc5d3e18cb6e97c1.jpg",
-            thumbnailUrl: null,
-            id: "5da95615dc5d3e18cb6e97c1"
-          },
-          {
-            actualSizeUrl:
-              "http://localhost:10000/devstoreaccount1/productimages/5dc5388a58de1e007b84c604/5da9561b25f3f623a4a07cd8.jpg",
-            thumbnailUrl: null,
-            id: "5da9561b25f3f623a4a07cd8"
-          }
-        ]
-      },
-      {
-        id: "5dc5388a58de1e007b84c60f",
-        name: "Featherlite Smart Mesh Revolving Office Chair",
-        minQuantity: "15pcs",
-        categoryId: 4,
-        subCategoryId: 12,
-        productTypeId: 23,
-        sellerId: "eb30dfce-a2a3-49ef-8f86-eda48c636f1b",
-        images: [
-          {
-            actualSizeUrl:
-              "http://localhost:10000/devstoreaccount1/productimages/5dc5388a58de1e007b84c60f/5da957ba530faab6c8e57316.jpg",
-            thumbnailUrl: null,
-            id: "5da957ba530faab6c8e57316"
-          },
-          {
-            actualSizeUrl:
-              "http://localhost:10000/devstoreaccount1/productimages/5dc5388a58de1e007b84c60f/5da957be67ce6a4d138aa598.jpg",
-            thumbnailUrl: null,
-            id: "5da957be67ce6a4d138aa598"
-          }
-        ]
-      },
-      {
-        id: "5dc5388a58de1e007b84c611",
-        name: "U-Type Conference Table",
-        minQuantity: "1",
-        categoryId: 4,
-        subCategoryId: 12,
-        productTypeId: 24,
-        sellerId: "eb30dfce-a2a3-49ef-8f86-eda48c636f1b",
-        images: [
-          {
-            actualSizeUrl:
-              "http://localhost:10000/devstoreaccount1/productimages/5dc5388a58de1e007b84c611/5da957db696b26d065ea7b93.jpg",
-            thumbnailUrl: null,
-            id: "5da957db696b26d065ea7b93"
-          },
-          {
-            actualSizeUrl:
-              "http://localhost:10000/devstoreaccount1/productimages/5dc5388a58de1e007b84c611/5da957e148f0d8ed711ca5f2.jpg",
-            thumbnailUrl: null,
-            id: "5da957e148f0d8ed711ca5f2"
-          },
-          {
-            actualSizeUrl:
-              "http://localhost:10000/devstoreaccount1/productimages/5dc5388a58de1e007b84c611/5da957e6426cdb42581a2c0b.jpg",
-            thumbnailUrl: null,
-            id: "5da957e6426cdb42581a2c0b"
-          }
-        ]
       }
     ],
     seller: [
       {
         id: "eb30dfce-a2a3-49ef-8f86-eda48c636f1b",
-        name: "Sketch Co"
+        name: "Sketch Co",
+        cityId: 1,
+        countryId: 1,
+        stateId: 2
       }
     ],
     categories: [
@@ -196,6 +124,24 @@ module.exports = () => {
         name: "Wooden Door",
         categoryId: 1,
         subCategoryId: 4
+      }
+    ],
+    cities: [
+      {
+        id: 1,
+        name: "Mumbai"
+      }
+    ],
+    countries: [
+      {
+        id: 1,
+        name: India
+      }
+    ],
+    state: [
+      {
+        id: 2,
+        name: "Maharashtra"
       }
     ]
   };
